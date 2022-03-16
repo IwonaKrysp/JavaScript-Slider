a5ab41c8dc49f7460003e1d9aac380c7173c66d3
IwonaKrysp <iwona.krysp@gmail.com>
2022-03-16 18:42:24 +0000

    complete

--- slideshow.js
+++ slideshow.js
@@ -1,81 +1,85 @@
-    let images = [
-      "masuria1.png",
-      "masuria2.jpeg",
-      "masuria3.jpeg",
-      "masuria4.jpeg",
-      "masuria5.jpeg",
-      "masuria6.jpeg",
-    ];
+let images = [
+  "masuria1.png",
+  "masuria2.jpeg",
+  "masuria3.jpeg",
+  "masuria4.jpeg",
+  "masuria5.jpeg",
+  "masuria6.jpeg",
+];
 
-    let count = 0;
-    let next = document.getElementById("nextBtn");
-    let back = document.getElementById("backBtn");
-    let slide = document.getElementById("slide");
-    slide.src = images[count];
+let count = 0;
+let nextButton = document.getElementById("nextBtn");
+let backButton = document.getElementById("backBtn");
+let imgBox = document.getElementById("img");
+imgBox.src = images[count];
 
-    let autoPlay = document.getElementById("autoPlay");
-    let stop = document.getElementById("stop");
-    let autoBack = document.getElementById("autoBack");
+let autoPlayButton = document.getElementById("autoPlay");
+let stopButton = document.getElementById("stop");
+let autoBackButton = document.getElementById("autoBack");
 
-    //___________next button___________________________
+//___________next button___________________________
 
-    next.addEventListener("click", function goNext() {
-      if (count < images.length - 1) {
-        count++;
-      } else {
-        count = 0;
-      }
+nextButton.addEventListener("click", function goNext() {
+  if (count < images.length - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  imgBox.src = images[count];
+});
 
-      slide.src = images[count];
-    });
+//____________back button__________________________
 
-    //____________back button__________________________
+backButton.addEventListener("click", function goBack() {
+  if (count > 0) {
+    count--;
+  } else {
+    count = images.length - 1;
+  }
+  imgBox.src = images[count];
+});
 
-    back.addEventListener("click", function goBack() {
-      if (count > 0) {
-        count--;
-      } else {
-        count = images.length - 1;
-      }
+//____________play button_______________________
 
-      slide.src = images[count];
-    });
+function play() {
+  if (count < images.length - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  imgBox.src = images[count];
+}
 
-    //____________play button_______________________
+//check if there is already one set and use clearInterval
+let forward;
+autoPlayButton.addEventListener("click", function () {
+  //if theres not setInterval
+  if (forward === undefined) {
+    forward = setInterval(play, 1000);
+  }
+});
 
-    let forward;
-    autoPlay.addEventListener("click", function play() {
-      if (count < images.length - 1) {
-        count++;
-      } else {
-        count = 0;
-      }
+stopButton.addEventListener("click", function stop() {
+  clearInterval(forward);
+  forward = undefined;
+  clearInterval(backward);
+  backward = undefined;
+});
 
-      slide.src = images[count];
-      forward = setTimeout(play, 1000);
+//_____________play back button______________________
 
-      
-    });
+function playBack() {
+  if (count > 0) {
+    count--;
+  } else {
+    count = images.length - 1;
+  }
+  imgBox.src = images[count];
+}
 
-
-     stop.addEventListener("click", function stop() {
-       clearTimeout(forward);
-       clearTimeout(backward);
-     });
-
-
-    //_____________play back button______________________
-
-    let backward;
-    autoBack.addEventListener("click", function playBack() {
-      if (count > 0) {
-        count--;
-      } else {
-        count = images.length - 1;
-      }
-
-      slide.src = images[count];
-      backward = setTimeout(playBack, 1000);
-    });
-
-   
+let backward;
+autoBackButton.addEventListener("click", function () {
+  if (backward === undefined) {
+    backward = setInterval(playBack, 1000);
+  }
+});
